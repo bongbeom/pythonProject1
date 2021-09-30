d07d6520f6b036aa0cd48d82633565e505b6ecff
bongbeom <qhdqja97@gmai.com>
2021-09-30 16:21:40 +0900

    Updates on Local Master Branch

--- main.py
+++ main.py
@@ -16,5 +16,8 @@ print("Hello World 4")
 #adding new item in master branch
 print("Hello World 5")
 
+#updates in local master branch
+print("Hello World Updates on Local Master Branch")
+
 #updates in main branch of github
 print("Hello World Update on Github Branch")
